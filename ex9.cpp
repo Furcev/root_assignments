5b6bae1a9bd330b66851c05418ba923f69edd192
Furcev <60572621+Furcev@users.noreply.github.com>
2024-12-29 11:46:04 +0700

    ex9

--- ex9.cpp
+++ ex9.cpp
@@ -1,118 +1,91 @@
 #include <iostream>
 #include <TTree.h>
-#include <TLorentzVector.h>
-#include <TH1F.h>
-#include <TCanvas.h>
 #include <TFile.h>
-#include <TMath.h>
 #include <TGraphErrors.h>
+#include <TCanvas.h>
+#include <TMath.h>
+#include <TLorentzVector.h>
+#include <TVector3.h>
+#include <vector>
 
-using namespace TMath;
+void analyze_photons(){
+    TFile *inputFile = TFile::Open("newroot.root", "READ");
 
-void analyze_photon_candidates(const char* inputFileName, const char* outputDir) {
-    // Открываем входной файл
-    TFile *inputFile = TFile::Open(inputFileName);
-    TTree *tree = (TTree*)inputFile->Get("MyTree");
+    TFile *outputFile = new TFile("new.root", "RECREATE");
+    
+    TTree *tree = (TTree *)inputFile->Get("MyTree");
 
-    // Переменные для чтения данных из дерева
-    Int_t numPhotons;
-    Float_t energyPhoton[300], thetaPhoton[300], phiPhoton[300];
+    int numPhotons;
+    float energyPhoton[40], thetaPhoton[40], phiPhoton[40];
+
+    std::vector<float> thetaPhotonGraph(18, 0);
+    std::vector<float> phiPhotonGraph(36, 0);
 
     tree->SetBranchAddress("nph", &numPhotons);
     tree->SetBranchAddress("eph", energyPhoton);
     tree->SetBranchAddress("thetaph", thetaPhoton);
     tree->SetBranchAddress("phiph", phiPhoton);
 
-    // Создаем выходной файл и гистограммы
-    TFile *outputFile = new TFile(Form("%s/output.root", outputDir), "RECREATE");
-    
-    // Гистограммы для полярного и азимутального углов
-    TH1F *histPolarAngle = new TH1F("histPolarAngle", "Polar Angle Distribution; Polar Angle (rad); Number of Candidates", 36, 0, TMath::Pi());
-    TH1F *histAzimuthalAngle = new TH1F("histAzimuthalAngle", "Azimuthal Angle Distribution; Azimuthal Angle (rad); Number of Candidates", 36, 0, 2 * TMath::Pi());
+    // График для theta
+    TCanvas *canvasTheta = new TCanvas("canvasTheta", "Candidates vs #theta", 600, 500);
+    TGraphErrors *graphTheta = new TGraphErrors();
+    graphTheta->SetTitle("Candidates vs #theta");
 
-    Int_t totalCandidates = 0;
+    // График для phi
+    TCanvas *canvasPhi = new TCanvas("canvasPhi", "Candidates vs #phi", 600, 500);
+    TGraphErrors *graphPhi = new TGraphErrors();
+    graphPhi->SetTitle("Candidates vs #phi");
 
-    // Цикл по событиям
-    Long64_t totalEntries = tree->GetEntries();
-    for (Long64_t i = 0; i < totalEntries; i++) {
+    // Главный цикл обработки событий
+    for (int i = 0; i < tree->GetEntries(); i++) {
         tree->GetEntry(i);
-        
-        TLorentzVector photon1(0, 0, 0, 0), photon2(0, 0, 0, 0);
-        
-        // Цикл по парам фотонов
-        for (Int_t j = 0; j < (numPhotons - 1); j++) {
-            for (Int_t k = j + 1; k < numPhotons; k++) {
-                photon1.SetPx(energyPhoton[j] * Sin(thetaPhoton[j]) * Cos(phiPhoton[j]));
-                photon1.SetPy(energyPhoton[j] * Sin(thetaPhoton[j]) * Sin(phiPhoton[j]));
-                photon1.SetPz(energyPhoton[j] * Cos(thetaPhoton[j]));
-                photon1.SetE(energyPhoton[j]);
+        for (int j = 0; j < numPhotons - 1; j++) {
+            for (int k = j + 1; k < numPhotons; k++) {
+                TLorentzVector photon1(energyPhoton[j], 0, 0, energyPhoton[j]);
+                photon1.SetTheta(thetaPhoton[j]);
+                photon1.SetPhi(phiPhoton[j]);
 
-                photon2.SetPx(energyPhoton[k] * Sin(thetaPhoton[k]) * Cos(phiPhoton[k]));
-                photon2.SetPy(energyPhoton[k] * Sin(thetaPhoton[k]) * Sin(phiPhoton[k]));
-                photon2.SetPz(energyPhoton[k] * Cos(thetaPhoton[k]));
-                photon2.SetE(energyPhoton[k]);
+                TLorentzVector photon2(energyPhoton[k], 0, 0, energyPhoton[k]);
+                photon2.SetTheta(thetaPhoton[k]);
+                photon2.SetPhi(phiPhoton[k]);
 
-                Float_t invariantMass = (photon1 + photon2).M(); // Вычисление инвариантной массы
-                
-                // Проверка инвариантной массы
-                if (invariantMass >= 0.1 && invariantMass <= 0.2) {
-                    // Заполнение гистограмм
-                    histPolarAngle->Fill(thetaPhoton[j]);
-                    histPolarAngle->Fill(thetaPhoton[k]);
-                    histAzimuthalAngle->Fill(phiPhoton[j]);
-                    histAzimuthalAngle->Fill(phiPhoton[k]);
-                    totalCandidates++;
+                TLorentzVector photonSum = photon1 + photon2;
+
+                TVector3 vector1(sin(thetaPhoton[j]) * cos(phiPhoton[j]), sin(thetaPhoton[j]) * sin(phiPhoton[j]), cos(thetaPhoton[j]));
+                TVector3 vector2(sin(thetaPhoton[k]) * cos(phiPhoton[k]), sin(thetaPhoton[k]) * sin(phiPhoton[k]), cos(thetaPhoton[k]));
+                float angle = vector1.Angle(vector2);
+
+                double invariantMass = sqrt(2. * energyPhoton[j] * energyPhoton[k] * (1. - cos(angle)));
+                if (0.1 <= invariantMass && invariantMass <= 0.2) {
+                    int thetaIndex = photonSum.Theta() * 18 / TMath::Pi();
+                    int phiIndex = (photonSum.Phi() + TMath::Pi()) * 18 / TMath::Pi();
+                    thetaPhotonGraph[thetaIndex]++;
+                    phiPhotonGraph[phiIndex]++;
                 }
             }
         }
     }
 
-    std::cout << "Total candidates found: " << totalCandidates << std::endl;
-
-    // Создание графиков с ошибками
-    TGraphErrors *graphPolarAngle = new TGraphErrors();
-    TGraphErrors *graphAzimuthalAngle = new TGraphErrors();
-
-    for (int bin = 1; bin <= histPolarAngle->GetNbinsX(); bin++) {
-        double x = histPolarAngle->GetBinCenter(bin);
-        double y = histPolarAngle->GetBinContent(bin);
-        double error = TMath::Sqrt(y); // Ошибка по квадратному корню из количества
-        graphPolarAngle->SetPoint(bin - 1, x, y);
-        graphPolarAngle->SetPointError(bin - 1, 0, error);
+    for (int i = 0; i < 18; i++) {
+        graphTheta->SetPoint(i, i * 10, thetaPhotonGraph[i]);
+        graphTheta->SetPointError(i, 0, sqrt(thetaPhotonGraph[i]));
     }
+    graphTheta->SetMarkerColor(kGreen); 
+    graphTheta->SetMarkerStyle(21);
+    graphTheta->SetMarkerSize(0.5); 
 
-    for (int bin = 1; bin <= histAzimuthalAngle->GetNbinsX(); bin++) {
-        double x = histAzimuthalAngle->GetBinCenter(bin);
-        double y = histAzimuthalAngle->GetBinContent(bin);
-                double error = TMath::Sqrt(y); // Ошибка по квадратному корню из количества
-        graphAzimuthalAngle->SetPoint(bin - 1, x, y);
-        graphAzimuthalAngle->SetPointError(bin - 1, 0, error);
+    for (int i = 0; i < 36; i++) {
+        graphPhi->SetPoint(i, i * 10, phiPhotonGraph[i]);
+        graphPhi->SetPointError(i, 0, sqrt(phiPhotonGraph[i]));
     }
+    graphPhi->SetMarkerColor(kMagenta); 
+    graphPhi->SetMarkerStyle(21);
+    graphPhi->SetMarkerSize(0.5); 
 
-    // Создание канваса и рисование графиков
-    TCanvas *canvas = new TCanvas("canvas", "Photon Candidates Analysis", 1200, 600);
-    canvas->Divide(2, 1);
+    canvasTheta->cd();
+    graphTheta->Draw("APC"); 
+    canvasTheta->BuildLegend(0.48, 0.8, 0.9, 0.9);
 
-    canvas->cd(1);
-    graphPolarAngle->SetMarkerStyle(21); // Квадратные маркеры
-    graphPolarAngle->SetMarkerSize(0.5);
-    graphAzimuthalAngle->SetLineColor(kRed);
-    graphPolarAngle->SetTitle("Polar Angle Distribution; Polar Angle (rad); Number of Candidates");
-    graphPolarAngle->Draw("AP");
-    
-    canvas->cd(2);
-    graphAzimuthalAngle->SetMarkerStyle(21); // Квадратные маркеры
-    graphAzimuthalAngle->SetMarkerSize(0.5);
-    graphAzimuthalAngle->SetLineColor(kRed);
-    graphAzimuthalAngle->SetTitle("Azimuthal Angle Distribution; Azimuthal Angle (rad); Number of Candidates");
-    graphAzimuthalAngle->Draw("AP");
-
-    // Добавление легенды
-    TLegend *legend = new TLegend(0.7, 0.7, 0.9, 0.9);
-    legend->AddEntry(graphPolarAngle, "Candidates in Polar Angle", "p");
-    legend->AddEntry(graphAzimuthalAngle, "Candidates in Azimuthal Angle", "p");
-    legend->Draw();
-
-    // Сохранение графиков в файл
-    canvas->SaveAs(Form("%s/pion_candidates_analysis.png", outputDir));
+    canvasPhi->cd();
+    graphPhi->Draw("APC"); 
 }
